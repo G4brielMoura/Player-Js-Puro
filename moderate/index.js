d00773da5f2a76e40c1fd98ee1a73ee73e08011e
G4brielMoura <pereirbiel@gmail.com>
2024-11-01 10:04:52 -0300

    js-setings

--- moderate/index.js
+++ moderate/index.js
@@ -1,15 +1,21 @@
-const title = document.querySelector(".card-content h5")
-const artist = document.querySelector(".artist")
-const cover = document.querySelector(".card-image")
-const audio = document.querySelector("audio")
-
-const data = {
+const player = {
+title: document.querySelector(".card-content h5"),
+artist: document.querySelector(".artist"),
+cover: document.querySelector(".card-image"),
+audio: document.querySelector("audio"),
+data: {
   title: "SEX MACHINE",
   artist: "James Brown",
   cover: "files/1.png",
   file: "files/Sexmachine.mp3"
+},
+start() {
+player.cover.style.background = `url('${player.data.cover}') no-repeat center center / cover`;
+player.title.innerText = player.data.title;
+player.artist.innerText = player.data.artist;
+player.audio.src = player.data.file;
+
 }
-cover.style.background = `url('${data.cover}') no-repeat center center / cover`
-title.innerText = data.title
-artist.innerText = data.artist
-audio.src = data.file
+};
+
+player.start();
